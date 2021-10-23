d4984a196f3b0c07dca04f1bd3624b292b806b94
Сергей Субботин <zarodiny@yandex.ru>
2021-10-23 17:17:36 +1000

    добавлен модуль scale

--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ import './data.js';
 import './createPictures.js';
 import './createBigPictures.js';
 import './form.js';
-
+import './scale.js';
---
+++ js/scale.js
@@ -0,0 +1,34 @@
+const zoomValue = {
+  MIN: 25,
+  MAX: 100,
+  STEP: 25,
+};
+
+const scaleControlls = document.querySelector('.img-upload__scale');
+
+const scaleControllSmallerButton = scaleControlls.querySelector('.scale__control--smaller');
+const scaleControllBiggerButton = scaleControlls.querySelector('.scale__control--bigger');
+const scaleControllValue = scaleControlls.querySelector('.scale__control--value');
+
+const formImage = document.querySelector('.img-upload__form');
+const sizeImg = formImage.querySelector('img');
+
+scaleControllSmallerButton.addEventListener('click', () => {
+  let size = parseInt(scaleControllValue.value, 10);
+  if (size === zoomValue.MIN) {
+    return;
+  }
+  size -= zoomValue.STEP;
+  scaleControllValue.value = `${size}%`;
+  sizeImg.style.transform = `scale(${size / 100})`;
+});
+
+scaleControllBiggerButton.addEventListener('click', () => {
+  let size = parseInt(scaleControllValue.value, 10);
+  if (size === zoomValue.MAX) {
+    return;
+  }
+  size += zoomValue.STEP;
+  scaleControllValue.value = `${size}%`;
+  sizeImg.style.transform = `scale(${size / 100})`;
+});
